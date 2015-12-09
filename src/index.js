4c5507f928906430eca3f4ac8f8fb507c38fe6d5
Keith Cirkel <github@keithcirkel.co.uk>
2015-12-09 13:24:40 +0000

    feat: initial implementation

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import iniFile from 'packagesmith.formats.ini';
+import multiline from 'packagesmith.formats.multiline';
+import repositoryQuestion from 'packagesmith.questions.repository';
+import defaultsDeep from 'lodash.defaultsdeep';
+import { runProvisionerSet } from 'packagesmith';
+export function provisionGit({
+  init = true,
+  setupGitConfig = true,
+  gitConfigOptions,
+  gitignoreLines = false,
+  gitIgnoreOptions,
+} = {}) {
+  const provision = {};
+  if (init) {
+    provision['.git'] = {
+      type: 'folder',
+      command: 'git init -q',
+    };
+  }
+  if (Array.isArray(gitignoreLines) && gitignoreLines.length) {
+    provision['.gitignore'] = {
+      contents: multiline((gitIgnore) => [ ...gitIgnore, ...gitignoreLines ], gitIgnoreOptions),
+    };
+  }
+  if (setupGitConfig) {
+    provision['.git/config'] = {
+      questions: [ repositoryQuestion() ],
+      contents: iniFile((gitConfig, { repository }) => defaultsDeep(gitConfig, {
+        'remote "origin"': {
+          url: repository,
+          fetch: '+refs/heads/*:refs/remotes/origin/*',
+        },
+      }, gitConfig), gitConfigOptions),
+    };
+  }
+  return provision;
+}
+export default provisionGit;
+
+if (require.main === module) {
+  runProvisionerSet(process.argv[2] || '.', provisionGit());
+}
